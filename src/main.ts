ef1cbcff34481609e8f72bb57e0a1b2e33995ed6
ArleysGatica <gatica.arleys16@gmail.com>
2025-04-28 18:11:02 -0600

    add Swagger API support and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Gym Backend API')
+    .setDescription(
+      'Documentación de la API para gestión de clientes, productos, entrenadores y pagos',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3003);
 }
 bootstrap();
