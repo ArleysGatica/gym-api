5337a28f37599f46076f3dbace4418dca6f537f3
ArleysGatica <gatica.arleys16@gmail.com>
2025-04-30 00:58:07 -0600

    feat(auth): add AuthModule to app imports and configure JWT bearer authentication

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,16 @@ async function bootstrap() {
       'API para gestión de clientes, productos, entrenadores y pagos',
     )
     .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        in: 'header',
+      },
+      'access-token',
+    )
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
